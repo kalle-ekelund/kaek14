7c8ae86b5e2dba0f3348f8f7338962a3c7cce8d6
Kalle Ekelund <kalle.ekelund.90@gmail.com>
2015-01-13 18:32:55 +0100

    Detta är rätt väg Travis!!! Skaffa gps för fan

--- test/config.php
+++ test/config.php
@@ -1,4 +1,4 @@
 <?php
-include __DIR__ . "/autoloader.php";
-include __DIR__ ."/../autoload.php";
-include __DIR__ ."/../../webroot/config_with_app.php";
+include __DIR__ . "/../autoloader.php";
+include __DIR__ ."/../../autoload.php";
+include __DIR__ ."/../../../webroot/config_with_app.php";
